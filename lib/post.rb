e7a28bd723f1e48fdc30ca5c136f1c37aa58fd72
Brad Schrag <brad.schrag@gmail.com>
2018-11-20 03:21:41 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,14 @@
 class Post
   attr_accessor :title, :author
 
+  @@all_posts = []
+
   def initialize(title)
     @title = title
+    @@all_posts << self
+  end
+
+  def self.all_posts
+    @@all_posts.count
   end
 end
