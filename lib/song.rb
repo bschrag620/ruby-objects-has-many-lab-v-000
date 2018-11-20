3f579995716853808d9773041a6bbb2a566a271f
Brad Schrag <brad.schrag@gmail.com>
2018-11-20 03:14:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all_songs = []
+
   def initialize(name)
     @name = name
+    @@all_songs << self
   end
 
+  def self.song_count
+    @@all_songs.count
+  end
 end
